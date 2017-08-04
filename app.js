f32b1e03c894cd331d63f374f3175cb1adedab72
Shaikh Farhan <98farhan94@gmail.com>
2017-08-04 20:21:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var request = require('request');
 var fs = require('fs');
 var path = require('path');
 
-['SLACK_TOKEN', 'RPCUSER', 'RPCPASSWORD', 'MONGODB_URL'].forEach(function (envVar) {
+['SLACK_TOKEN',].forEach(function (envVar) {
   if (!process.env[envVar]) {
     throw new Error(envVar + ' env var required');
   }
@@ -11,7 +11,7 @@ var path = require('path');
 
 var slackbot = new SlackBot({
   token: process.env.SLACK_TOKEN,
-  name: 'wunderbot'
+  name: 'PriceBot'
 });
 
 
@@ -23,26 +23,13 @@ function sendWelcomeMessage(user) {
   });
 };
 
-var hashbot = require('./bots/hashbot');
-hashbot.init(slackbot, process.env.MINING_CHANNEL);
-
-var claimbot = require('./bots/claimbot');
-claimbot.init(slackbot, process.env.CLAIMS_CHANNEL, process.env.RPCUSER, process.env.RPCPASSWORD, process.env.MONGODB_URL);
-
 var pricebot = require('./bots/pricebot');
 pricebot.init(process.env.MARKET_TRADING_CHANNEL);
 
-var modbot = require('./bots/modbot');
-modbot.init(process.env.MONGODB_URL, process.env.SLACK_TOKEN, slackbot);
 
 slackbot.on('start', function() {
   slackbot.on('message', function(data) {
-    modbot.check(slackbot,data);
-    if (data.type == 'team_join') {
-      setTimeout(function() { sendWelcomeMessage(data.user.id); }, 2000); //Delay because of slow slack api updates which sometimes does not send msg.
-    }
     if (data.text) {
-//      gifbot.handle_msg(data.text, data.channel);
 
       var command = data.text.trim().split(' ')[0];
 
@@ -58,15 +45,6 @@ slackbot.on('start', function() {
           'My code is at https://github.com/lbryio/lbry-wunderbot. I love learning new tricks.\n';
 
         slackbot.postMessage(data.channel, helpMsg, {icon_emoji: ':bulb:'});
-      }
-
-      if (command === hashbot.command) {
-        hashbot.respond(slackbot, data);
-      }
-
-      if (command === '!tip' && data.channel.startsWith("D")) {
-        var tipMsg = 'Sorry, tipping is now handled by <@tipbot>\n';
-        slackbot.postMessage(data.channel, tipMsg, {icon_emoji: ':bulb:'});
       }
 
       if (command === pricebot.command) {
