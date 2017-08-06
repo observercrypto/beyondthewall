e2ef08ec6088ef4a19669fb02e2a53415ec4a0e9
Shaikh Farhan <98farhan94@gmail.com>
2017-08-06 07:12:02 +0530

    Update statbot.js

--- bots/statbot.js
+++ bots/statbot.js
@@ -113,7 +113,7 @@ function formatMessage(amount, rate, option) {
     (option == 'USD') ? cur = '$' : (option == 'BTC') ? cur = 'BTC' : cur = 'ETH';
     var value = numeral(rate.rate * amount);
     var value = formaty(value,4, cur);
-    return '*' + numeral(amount).format('0,0[.][00000000]') + ' :dnt: = ' + value + '*';
+    return '*' + numeral(amount).format('0,0[.][00000000]') + ' :dnt: = ' + value + '*' +option;
 }
 
 function formaty(n, decimals, currency) {
